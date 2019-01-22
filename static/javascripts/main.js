cb8b9fc5599dc0b53ceef7549d35ee92c81c2a66
notikki <urashin.dmt@gmail.com>
2019-01-22 17:20:04 +0700

    upate index 3

--- static/javascripts/main.js
+++ static/javascripts/main.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-	
+    let menuBtn = document.getElementById('menu-btn');
+    let popup = document.getElementById('popup');
+    let menuPopup = document.getElementById('menu-popup');
+
+    function actionShowPopup() {
+        popup.classList.add('active');
+        menuPopup.classList.add('active');
+    }
+
+    function removeShowPopup() {
+        popup.classList.remove('active');
+        popup.classList.remove('active');
+    }
+
+    menuBtn.addEventListener('click', actionShowPopup);
+    popup.addEventListener('click', removeShowPopup);
 }, false);
